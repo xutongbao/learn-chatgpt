d570ff5d77b7e491e980e769ffca5518d9557e09
xutongbao@douyuxingchen.com <xutongbao@douyuxingchen.com>
2023-04-08 21:00:31 +0800

    1

--- src/views/ai/single/me/gptFour/Index.js
+++ src/views/ai/single/me/gptFour/Index.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import { Collapse, Image } from 'antd'
 import useList from './useList'
 import './index.css'
-
-const { Panel } = Collapse
 
 function Index(props) {
   // eslint-disable-next-line
@@ -13,25 +10,18 @@ function Index(props) {
   return (
     <>
       <div className="m-single-page-space">
-        <Collapse>
-          <Panel header="对比GPT-4与gpt-3.5-turbo对同一个问题的的回答" key="0">
-            <div>
-              <div>我的蓝牙耳机坏了，我是去挂号看牙科呢还是耳鼻咽喉科？</div>
-              <div className="m-single-question-img-wrap">
-                <Image
-                  className="m-single-question-img"
-                  src="http://static.xutongbao.top/img/m-gpt-4-1.jpg?time=20230404"
-                />
-              </div>
-              <div className="m-single-question-img-wrap">
-                <Image
-                  className="m-single-question-img"
-                  src="http://static.xutongbao.top/img/m-gpt-4-2.jpg?time=20230404"
-                />
-              </div>
-            </div>
-          </Panel>
-        </Collapse>
+        <div className='m-single-join-group-intro'>
+          1. 我的GPT-4接入之旅
+          <div>
+            {/* eslint-disable-next-line */}
+            <a
+              href={`https://blog.csdn.net/xutongbao/article/details/129851721?spm=1001.2014.3001.5501`}
+              target="_blank"
+            >
+              https://blog.csdn.net/xutongbao/article/details/129851721
+            </a>
+          </div>
+        </div>
       </div>
     </>
   )
